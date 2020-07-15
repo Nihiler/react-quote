33aab41b0ca90b2dcd8435ccf04b3d5371ffa781
julio <julioperezrdlf@gmail.com>
2020-07-15 20:13:18 +0200

    env api key

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    let [responseData, setResponseData] = React.useState('');   // new
+
+    React.useEffect(() => {
+        setResponseData('hello')
+        console.log(responseData)
+    }, [setResponseData, responseData])
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Fetching Data with React Hooks
+        </h1>
       </header>
     </div>
   );
